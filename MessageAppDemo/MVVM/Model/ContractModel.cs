c9495db4f154c91c0e5b595a1c15a1cf26e88336
levi73159 <levidalrymple1221@gmail.com>
2021-09-04 16:12:44 -0500

    HalfWay

---
+++ MessageAppDemo/MVVM/Model/ContractModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace MessageAppDemo.MVVM.Model
+{
+	public class ContractModel
+	{
+		public string Username { get; set; }
+		public string ImageSource { get; set; }
+		public ObservableCollection<MessageModel> Messages { get; set; }
+		public string LastMessage
+		{
+			get { return Messages.Last().Message; }
+		}
+	}
+}
---
+++ MessageAppDemo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MessageAppDemo
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void ExitButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		private void Border_OnMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			if(e.LeftButton == MouseButtonState.Pressed)
+				DragMove();
+		}
+
+		private void MinimizeButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Minimized;
+		}
+
+		private void MaximizeButton_OnClick(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState == WindowState.Maximized
+					? WindowState.Normal
+					: WindowState.Maximized;
+		}
+	}
+}
