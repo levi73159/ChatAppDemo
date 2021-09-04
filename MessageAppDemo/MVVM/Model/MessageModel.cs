e302fe2aebecff9bc577c00c152e80e463c7df8c
levi73159 <levidalrymple1221@gmail.com>
2021-09-04 16:12:44 -0500

    HalfWay

---
+++ MessageAppDemo/MVVM/Model/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MessageAppDemo.MVVM.Model
+{
+	public class MessageModel
+	{
+		public string Username { get; set; }
+		public string UsernameColor { get; set; }
+		public string ImageSource { get; set; }
+		public string Message { get; set; }
+		public DateTime Time { get; set; }
+		public bool IsNativeOrigin { get; set; }
+		public bool? FirstMessage { get; set; }
+	}
+}
---
+++ MessageAppDemo/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using MessageAppDemo.MVVM.Model;
+
+namespace MessageAppDemo.MVVM.ViewModel
+{
+	public class MainViewModel
+	{
+		public ObservableCollection<MessageModel> Messages { get; set; }
+		public ObservableCollection<ContractModel> Contracts { get; set; }
+
+		public MainViewModel()
+		{
+			Messages = new ObservableCollection<MessageModel>();
+			Contracts = new ObservableCollection<ContractModel>();
+			
+			Messages.Add(new MessageModel
+			{
+				Username = "Allison",
+				UsernameColor = "#409aff",
+				ImageSource = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/249px-Video-Game-Controller-Icon-IDV-green.svg.png",
+				Message = "Test",
+				Time = DateTime.Now,
+				IsNativeOrigin = false,
+				FirstMessage = true
+			});
+
+			for (int i = 0; i < 3; i++)
+			{
+				Messages.Add(new MessageModel
+				{
+					Username = "Allison",
+					UsernameColor = "#409aff",
+					ImageSource = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/249px-Video-Game-Controller-Icon-IDV-green.svg.png",
+					Message = "Test",
+					Time = DateTime.Now,
+					IsNativeOrigin = false,
+					FirstMessage = false
+				});
+			}
+			
+			for (int i = 0; i < 4; i++)
+			{
+				Messages.Add(new MessageModel
+				{
+					Username = "Alison",
+					UsernameColor = "#0fdbff",
+					ImageSource = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/249px-Video-Game-Controller-Icon-IDV-green.svg.png",
+					Message = "Love You",
+					Time = DateTime.Now,
+					IsNativeOrigin = true,
+				});
+			}
+			
+			Messages.Add(new MessageModel
+			{
+				Username = "Alison",
+				UsernameColor = "#0fdbff",
+				ImageSource = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/249px-Video-Game-Controller-Icon-IDV-green.svg.png",
+				Message = "Last",
+				Time = DateTime.Now,
+				IsNativeOrigin = true,
+			});
+
+			for (int i = 0; i < 5; i++)
+			{
+				Contracts.Add(new ContractModel
+				{
+					Username = $"Allison {i}",
+					ImageSource = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Video-Game-Controller-Icon-IDV-green.svg/249px-Video-Game-Controller-Icon-IDV-green.svg.png",
+					Messages = Messages
+				});
+			}
+		}
+	}
+}
